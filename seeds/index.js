b393ae0099dd2afe242132914408d7d9231ae301
crypaipal <msamokar228@gmail.com>
2025-01-14 00:20:02 +0100

    Deploying!

--- seeds/index.js
+++ seeds/index.js
@@ -1,40 +1,35 @@
-const citiesData = require('./cities');
-const { sequelize } = require('../config/database');
-const { gymNames } = require("./gymHelpers");
-const { gymDescriptions } = require("./gymDescriptions");
-const Gym = require('../models/gym');
-const User = require("../models/user");
-
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await sequelize.sync();
     await Gym.destroy({ where: {} });
 
-    const users = await User.findAll();
+    let users = await User.findAll();
+
+    if (users.length === 0) {
+        await User.bulkCreate([
+            { email: "malpa@gmail.com", username: "Malpa", password: "123" },
+            { email: "cry@gmail.com", username: "Cry", password: "123" },
+            { email: "matvey@gmail.com", username: "Maciej", password: "123" },
+        ]);
+        users = await User.findAll();
+    }
 
     for (let i = 0; i < 200; i++) {
         const randomCities = Math.floor(Math.random() * 347);
         const price = Math.floor(Math.random() * 50) + 10;
-        let randomUser;
-        if(users.length > 0) {
-            randomUser = sample(users);
-        } else {
-            User.bulkCreate([{email: "malpa@gmail.com", username: "Malpa", password: 123},
-            {email: "cry@gmail.com", username: "Cry", password: 123},
-            {email: "matvey@gmail.com", username: "Maciej", password: 123}]);
-            users = await User.findAll();
-            randomUser = sample(users);
-        }
+        const randomUser = sample(users);
         const description = sample(gymDescriptions);
         const selectedCity = citiesData[randomCities];
 
-        const gym = await Gym.create({
+        await Gym.create({
             user_id: randomUser.id,
-            location: `${citiesData[randomCities].name}`,
+            location: `${selectedCity.name}`,
             name: `${sample(gymNames)}`,
-            images: [{ url: `https://images.unsplash.com/photo-1623874514711-0f321325f318?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGd5bXxlbnwwfHwwfHx8MA%3D%3D`,
-             filename: `random_image_${i}` }],
+            images: [{
+                url: `https://images.unsplash.com/photo-1623874514711-0f321325f318?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGd5bXxlbnwwfHwwfHx8MA%3D%3D`,
+                filename: `random_image_${i}`
+            }],
             description: description,
             price: price,
             geometry: {
@@ -43,7 +38,7 @@ const seedDB = async () => {
             }
         });
     }
-}
+};
 
 seedDB().then(() => {
     sequelize.close();
